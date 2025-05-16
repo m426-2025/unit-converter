d7419d42c9a294f957a7ef5669c5567272b1adc3
Eneas Infanger <eneas_infanger@sluz.ch>
2025-05-16 13:26:41 +0200

    Added more conversion tests

--- src/conversion.test.ts
+++ src/conversion.test.ts
@@ -1,5 +1,45 @@
-import { convertToMeters } from "./conversion";
+import { convertToMeters, Unit } from "./conversion";
 
-test("imperial units", () => {
-  expect(convertToMeters(1, "mile")).toBeCloseTo(1609.344);
+describe("imperial units", () => {
+  test.each([
+    [1, "league", 4828.032],
+    [1, "mile", 1609.344],
+    [1, "yard", 0.9144],
+    [1, "foot", 0.3048],
+    [1, "inch", 0.0254],
+  ])("%s %s in meters", (value: number, unit: Unit, expected) => {
+    expect(convertToMeters(value, unit)).toBeCloseTo(expected);
+  });
+
+  test.each([
+    [2, "league", 2 * 4828.032],
+    [0.5, "mile", 0.5 * 1609.344],
+    [10, "yard", 10 * 0.9144],
+    [3, "foot", 3 * 0.3048],
+    [100, "inch", 100 * 0.0254],
+  ])("%s %s in meters", (value: number, unit: Unit, expected) => {
+    expect(convertToMeters(value, unit)).toBeCloseTo(expected);
+  });
+});
+
+describe("metric units", () => {
+  test.each([
+    [1, "meter", 1],
+    [1, "centimeter", 0.01],
+  ])("%s %s in meters", (value: number, unit: Unit, expected) => {
+    expect(convertToMeters(value, unit)).toBeCloseTo(expected);
+  });
+
+  test.each([
+    [2, "meter", 2],
+    [50, "centimeter", 0.5],
+  ])("%s %s in meters", (value: number, unit: Unit, expected) => {
+    expect(convertToMeters(value, unit)).toBeCloseTo(expected);
+  });
+});
+
+test("unknown unit throws error", () => {
+  expect(() => convertToMeters(1, "unknown" as Unit)).toThrow(
+    "unknown unit: unknown",
+  );
 });
